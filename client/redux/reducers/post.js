a0ff68f71abfc483695ea81f1992476f0cbcd408
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-05-08 22:53:49 -0700

    add post.js

---
+++ client/redux/reducers/post.js
@@ -0,0 +1,20 @@
+// a post that is in active editing will live here
+// when 'closing', state will be cleared, and
+// added to the 'posts' arrray, in ./dashboard.js
+
+import { handleActions } from 'redux-actions'
+import {
+	CREATE_POST_REQUEST,
+	CREATE_POST_SUCCESS,
+	CREATE_POST_ERROR,
+  START_EDIT,
+	STOP_EDIT
+} from '../constants'
+
+const initialState = {
+  title: '',
+  text: '',
+  dateCreated: '',
+  isPublished: null,
+  lastEdited: null
+}
