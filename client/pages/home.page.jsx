0544962b8ea8e0e24c40d102dcf63991b0616f6b
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-05-15 23:30:18 -0700

    add aphrodite

--- client/pages/home.page.jsx
+++ client/pages/home.page.jsx
@@ -2,13 +2,25 @@
 
 import React from 'react'
 import Page from './page'
+import { StyleSheet, css } from 'aphrodite'
 
 const Home = () => {
 	return (
 		<Page>
-			<h1>Hello!</h1>
+			<h1 className={css(styles.orange, styles.small)}>Hello!</h1>
 		</Page>
 	)
 }
+
+const styles = StyleSheet.create({
+	orange: {
+		color: 'orange'
+	},
+	small: {
+		'@media (max-width: 600px)': {
+			backgroundColor: 'red'
+		}
+	}
+})
 
 export default Home
